890c71754c507e44bee3c057ba29babe3dc3fa46
Camilo Borja <camiloborja5@hotmail.com>
2022-09-01 11:33:29 -0500

    comentarios

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -6,17 +6,17 @@ const Form = () => {
   const [input, setInput] = useState('')
   const [pair, setPair] = useState('')
 
+// Estaba trabajando el el input pero encontre un bug inesperado y 
+// estaba trabajando en la corrección del mismo
   const handleSubmit = e => {
     e.preventDefault()
+    // setInput(e.target.value)
+    // // if (input.length % 2 == 0) {
+    // //   setPair('Numero par')
+    // // } else {
+    // //   setPair('Numero Impar')
+    // // }
   }
-  const inputPair = () => {
-    if (input.length % 2 == 0) {
-      setPair('Numero par')
-    } else {
-      setPair('Numero Impar')
-    }
-  }
-
   return (
     <div className='form'>
       <div className="form__text">
@@ -24,10 +24,10 @@ const Form = () => {
         <h2>SUSCRIBETE</h2>
         <span>Y enterate de todas las novedades</span>
       </div>
-      <form className='input__email' onSubmit={handleSubmit}>
+      <form className='input__email' onChange={handleSubmit} >
         <label htmlFor="email"></label>
-        <input id='email' type="email" placeholder='Ingresa tu email' />
-        <button onClick={inputPair}><i className='bx bx-right-arrow-alt'></i></button>
+        <input id='email' type="email" placeholder='Ingresa tu email'/>
+        <button><i className='bx bx-right-arrow-alt'></i></button>
         <span className='pair'>{pair}</span>
       </form>
     </div>
